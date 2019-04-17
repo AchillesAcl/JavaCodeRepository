76243c4c33050a5c2462e7171effbac4ddb9d6f9
achilles <463293012@qq.com>
2019-04-17 20:04:20 +0800

    adapter

---
+++ src/com.achilles/pattern/adapter/implement/Mp4Player.java
@@ -0,0 +1,15 @@
+package com.achilles.pattern.adapter.implement;
+
+import com.achilles.pattern.adapter.contract.AdvancedMediaPlayer;
+
+public class Mp4Player implements AdvancedMediaPlayer {
+    @Override
+    public void playVlc(String fileName) {
+
+    }
+
+    @Override
+    public void playMp4(String fileName) {
+        System.out.println("Playing mp4 file. Name: "+ fileName);
+    }
+}
